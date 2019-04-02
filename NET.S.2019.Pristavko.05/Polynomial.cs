ba92d4667c6ccdb93ba5d22ec2939e7c74b18f35
Prystauka Yauheni <34281826+Zheka1389@users.noreply.github.com>
2019-04-02 21:25:35 +0300

    1

---
+++ NET.S.2019.Pristavko.05/Polynomial.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2019.Pristavko._05
+{
+    public sealed class Polynomial
+    {
+        /// <summary>
+        /// The accuracy of counting
+        /// </summary>
+        public static double epsilon = 0.001;
+
+        private double[] coefficients;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Polynomial" /> class.
+        /// </summary>
+        /// <param name="coef">The coefficient</param>
+        /// <remarks>
+        /// (Uncorrect: 0*x^3 + 0*x^2+ 0*x + 0)
+        /// Nulcoef avoids this problem.
+        /// </remarks>
+        public Polynomial(params double[] coef)
+        {
+            if (coef == null)
+            {
+                throw new ArgumentNullException("Polynomial can not be null", nameof(coef));
+            }
+            Degree = coef.Length - 1;
+            coefficients = new double[Degree + 1];
+            int nullCoef = 0;
+            for (int i = 0; i <= Degree; i++)
+            {
+                if (coef[i] == 0)
+                {
+                    nullCoef++;
+                }
+                this[i] = coef[i];
+            }
+
+            if (nullCoef == Degree + 1)
+            {
+                Degree = 0;
+                coefficients = new double[Degree + 1];
+                coefficients[0] = 0;
+            }
+        }
+
+        private Polynomial(int degree)
+        {
+            Degree = degree;
+            coefficients = new double[Degree + 1];
+        }
+
+        /// <summary>
+        /// Gets the degree.
+        /// </summary>
+        public int Degree { get; private set; }
+
+        /// <summary>
+        /// Indexer
+        /// </summary>
+        public double this[int i]
+        {
+            get
+            {
+                if (Degree > coefficients.Length)
+                {
+                    throw new ArgumentOutOfRangeException("Wrong index");
+                }
+                return coefficients[i];
+            }
+            private set
+            {
+                if (i < 0 && i >= coefficients.Length)
+                {
+                    throw new ArgumentOutOfRangeException("Wrong index");
+                }
+                coefficients[i] = value;
+            }
+        }
+
+        /// <summary>
+        /// Operator + overload for polynomial objects
+        /// </summary>
+        /// <param name="pFirst">First polynomial</param>
+        /// <param name="pSecond">Second polynomial</param>
+        /// <returns>
+        /// The sum of polynomials
+        /// </returns>
+        public static Polynomial operator +(Polynomial pFirst, Polynomial pSecond)
+        {
+            if (pFirst is null)
+            {
+                throw new ArgumentNullException("First polynomial can not be null");
+            }
+            if (pSecond is null)
+            {
+                throw new ArgumentNullException("Second potynomial ca not be null");
+            }
+
+            Polynomial larger, smaller;
+            if (pFirst.Degree > pSecond.Degree)
+            {
+                larger = pFirst.Clone();
+                smaller = pSecond.Clone();
+            }
+            else
+            {
+                larger = pSecond.Clone();
+                smaller = pFirst.Clone();
+            }
+
+            for (int i = 0; i <= smaller.Degree; i++)
+            {
+                larger[i] += smaller[i];
+            }
+
+            Polynomial resultPolynomial = new Polynomial(larger.coefficients);
+            return resultPolynomial;
+        }
+
+        /// <summary>
+        /// Operator - overload for polynomial objects
+        /// </summary>
+        /// <param name="pFirst">First polynomial</param>
+        /// <param name="pSecond">Second polynomial</param>
+        /// <returns>
+        /// The substract of polynomials.
+        /// </returns>
+        public static Polynomial operator -(Polynomial pFirst, Polynomial pSecond)
+        {
+            if (pFirst is null)
+            {
+                throw new ArgumentNullException("First polynomial can not be null");
+            }
+            if (pSecond is null)
+            {
+                throw new ArgumentNullException("Second potynomial ca not be null");
+            }
+
+            Polynomial larger, smaller;
+            if (pFirst.Degree > pSecond.Degree)
+            {
+                larger = pFirst.Clone();
+                smaller = pSecond.Clone();
+            }
+            else
+            {
+                larger = pSecond.Clone();
+                smaller = pFirst.Clone();
+            }
+
+            for (int i = 0; i <= smaller.Degree; i++)
+            {
+                larger[i] -= smaller[i];
+            }
+
+            Polynomial resultPolynomial = new Polynomial(larger.coefficients);
+            return resultPolynomial;
+        }
+
+        /// <summary>
+        /// Operator * overload for polynomial objects
+        /// </summary>
+        /// <param name="pFirst">First polynomial</param>
+        /// <param name="pSecond">Second polynomial</param>
+        /// <returns>
+        /// The multiplication of polynomials.
+        /// </returns>
+        public static Polynomial operator *(Polynomial pFirst, Polynomial pSecond)
+        {
+            Polynomial resultPolynomial = new Polynomial(pFirst.Degree + pSecond.Degree);
+            for (int i = 0; i <= pFirst.Degree; i++)
+            {
+                for (int j = 0; j <= pSecond.Degree; j++)
+                {
+                    resultPolynomial.coefficients[i + j] += pFirst[i] * pSecond[j];
+                }
+            }
+            return resultPolynomial;
+        }
+
+        /// <summary>
+        /// Operator == overload for polynomial objects
+        /// </summary>
+        /// <param name="pFirst">First polynomial</param>
+        /// <param name="pSecond">Second polynomial</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static bool operator ==(Polynomial pFirst, Polynomial pSecond)
+        {
+            if (ReferenceEquals(pFirst, pSecond))
+            {
+                return true;
+            }
+
+            if (pFirst is null || pSecond is null)
+            {
+                return false;
+            }
+
+            if (pFirst.Degree != pSecond.Degree)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i <= pFirst.Degree; i++)
+                {
+                    if (Math.Abs(pFirst[i] - pSecond[i]) >= epsilon)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Operator != overload for polynomial objects
+        /// </summary>
+        /// <param name="pFirst">First polynomial</param>
+        /// <param name="pSecond">Second polynomial</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static bool operator !=(Polynomial pFirst, Polynomial pSecond)
+        {
+            return !(pFirst.Degree == pSecond.Degree);
+        }
+
+        public override string ToString()
+        {
+            string stringRepresentation = null;
+            for (int i = 0; i <= Degree; i++)
+            {
+                if (this[i] == 0)
+                {
+                    continue;
+                }
+                if (stringRepresentation != null)
+                {
+                    if (this[i] > 0)
+                    {
+                        stringRepresentation += "+";
+                    }
+                }
+                if (i == 0)
+                {
+                    stringRepresentation += this[i];
+                    continue;
+                }
+                if (i == 1)
+                {
+                    stringRepresentation += this[i];
+                    stringRepresentation += $"x";
+                    continue;
+                }
+                if (this[i] != 1)
+                {
+                    if (this[i] == -1)
+                    {
+                        stringRepresentation += "-";
+                    }
+                    else
+                    {
+                        stringRepresentation += this[i];
+                    }
+                }
+                stringRepresentation += $"x^{i}";
+            }
+            return stringRepresentation;
+        }
+
+        /// <summary>
+        /// Equalses the specified polynomials.
+        /// </summary>
+        /// <param name="other">The other polynomial.</param>
+        public bool Equals(Polynomial other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+            if (coefficients.Length != other.coefficients.Length)
+            {
+                return false;
+            }
+            return this == other;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null)
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+            return Equals((Polynomial)obj);
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        public override int GetHashCode()
+        {
+            int hashCode = 0;
+            for (int i = 0; i <= Degree; i++)
+            {
+                hashCode += this[i].GetHashCode() * i.GetHashCode();
+            }
+            return hashCode;
+        }
+
+        private Polynomial Clone()
+        {
+            return new Polynomial
+            {
+                coefficients = coefficients,
+                Degree = Degree
+            };
+        }
+    }
+}
---
+++ NET.S.2019.Pristavko.05/Tests/PolynomialTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2019.Pristavko._05.Tests
+{
+    class PolynomialTests
+    {
+        [TestCase(new double[] { 1, 2, 3, 4 }, new double[] { 1, 2, 3, 4 }, new double[] { 0 })]
+        [TestCase(new double[] { 1.204, -2.569, 3.987, 4.879, -0.896, 9 }, new double[] { 1, -2, -3, 4 }, new double[] { 0.204, -0.569, 6.987, 0.879, -0.896, 9 })]
+        public static void Polynomial_Substract(double[] array1, double[] array2, double[] expected)
+        {
+            Polynomial p1 = new Polynomial(array1);
+            Polynomial p2 = new Polynomial(array2);
+            Polynomial p3 = new Polynomial(expected);
+            Assert.AreEqual(p1 - p2, p3);
+        }
+
+        [TestCase(new double[] { 1, 2, 3, 4 }, new double[] { 1, 2, 3, 4 }, new double[] { 1, 4, 10, 20, 25, 24, 16 })]
+        [TestCase(new double[] { -0.251, 0.652, -0.983, 4.11 }, new double[] { -89.23, 0.1278, 0.9873 }, new double[] { 22.396700, -58.2100, 87.548600, -366.21700, -0.4452002, 4.0578 })]
+        public static void Polynomial_Multiply(double[] array1, double[] array2, double[] expected)
+        {
+            Polynomial p1 = new Polynomial(array1);
+            Polynomial p2 = new Polynomial(array2);
+            Polynomial p3 = new Polynomial(expected);
+            Assert.AreEqual(p1 * p2, p3);
+        }
+
+        [TestCase(new double[] { 1, 2, 3, 4, 0, -1, 11.2564, -0.00001 }, ExpectedResult = "1+2x+3x^2+4x^3-x^5+11,2564x^6-1E-05x^7")]
+        public static string Polynomial_ToString(double[] array)
+        {
+            Polynomial p1 = new Polynomial(array);
+            return p1.ToString();
+        }
+
+        [TestCase(new double[] { 1, 2, 3, 4, 0, -1, 11.2564, -0.00001 }, new double[] { 1, 2, 3, 4, 0, -1, 11.2564, -0.00001 }, ExpectedResult = true)]
+        [TestCase(new double[] { 1, 2, 3, 4, 0, -1, 11.2564, -0.20 }, new double[] { 1, 2, 3, 4, 0, -1, 11.2564, -0.19 }, ExpectedResult = false)]
+        public static bool Polynomial_Equals(double[] array1, double[] array2)
+        {
+            Polynomial p1 = new Polynomial(array1);
+            Polynomial p2 = new Polynomial(array2);
+            return p1.Equals(p2);
+        }
+    }
+}
